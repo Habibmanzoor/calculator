fc8ec302dd5c4111adc4fa790f084725953b499d
Habibmanzoor <159249434+Habibmanzoor@users.noreply.github.com>
2025-01-13 19:53:39 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function getnumber(num) {
+    var result = document.getElementById('show');
+    result.value += num;
+}
+
+function clearresult() {
+    var result = document.getElementById('show');
+    result.value = "";
+}
+
+function getresult() {
+    var result = document.getElementById('show');
+    result.value = eval(result.value);
+}
